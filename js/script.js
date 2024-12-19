824d4e31ff5d6345134a79f259c28141546f2b39
Prince Patel <patelprince2598@gmail.com>
2024-12-19 10:54:47 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu-links");
+const icon = document.querySelector(".hamburger-icon");
+
+function toggleMenu() {
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
